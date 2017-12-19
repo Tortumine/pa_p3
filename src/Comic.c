87010fd7a4d2dd4c787e28816a341ab168314079
Tortumine <tortumine@gmail.com>
2017-12-19 15:20:17 +0100

    cost function add + extra function declaration

--- src/Comic.c
+++ src/Comic.c
@@ -3,6 +3,7 @@
 //
 
 #include <stddef.h>
+#include <math.h>
 #include "PNM.h"
 
 /* ------------------------------------------------------------------------- *
@@ -22,8 +23,7 @@
  *              configuration.
  * NULL         if an error occured
  * ------------------------------------------------------------------------- */
-size_t* wrapImages(const PNMImage** images, size_t nbImages, size_t comicWidth,
-                   size_t comicBorder)
+size_t* wrapImages(const PNMImage** images, size_t nbImages, size_t comicWidth,size_t comicBorder)
 {
 
 }
@@ -43,8 +43,51 @@ size_t* wrapImages(const PNMImage** images, size_t nbImages, size_t comicWidth,
  * image        The comic image
  * NULL         if an error occured
  * ------------------------------------------------------------------------- */
-PNMImage* packComic(const PNMImage** images, size_t nbImages, size_t comicWidth,
-                    size_t comicBorder)
+PNMImage* packComic(const PNMImage** images, size_t nbImages, size_t comicWidth, size_t comicBorder)
 {
 
+}
+
+/* ------------------------------------------------------------------------- *
+ * Compute the optimal positions of the images on the page.
+ *
+ * Computes the difference between the wanted comicWidth and
+ * the with obtained bt cells i to j.
+ *
+ * PARAMETERS
+ * images       An array of images
+ * i            First image to consider
+ * j            Last image to consider
+ * comicWidth   The width of a page
+ * comicBorder  The border around images
+ *
+ * RETURN
+ * extras       The difference between desired length and the obtained one
+ * NULL         if an error occurred
+ * ------------------------------------------------------------------------- */
+int extras(const PNMImage** images,int i, int j, size_t comicWidth, size_t comicBorder)
+{
+
+}
+
+/* ------------------------------------------------------------------------- *
+ *
+ * Computes the cost of placing the cells i to j on the same row.
+ *
+ * PARAMETERS
+ * images       An array of images
+ * i            First image to consider
+ * j            Last image to consider
+ * comicWidth   The width of a page
+ * comicBorder  The border around images
+ *
+ * RETURN
+ * extras       The cost of the line
+ * NULL         if an error occurred
+ * ------------------------------------------------------------------------- */
+int cost(const PNMImage** images,int i, int j, size_t comicWidth, size_t comicBorder)
+{
+    int tmp = extras(images,i,j,comicWidth,comicBorder);
+    if(tmp==NULL)return NULL;
+    return pow(tmp,3);
 }
